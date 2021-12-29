22e570e4975c561c38403b7dfa29b97d21a4d670
Kira Hartlage <kira.hartlage@gmail.com>
2021-12-29 11:30:03 -0500

    todo app tutorial

---
+++ Services/Database.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Todo.Models;
+
+namespace Todo.Services
+{
+    public class Database
+    {
+        public IEnumerable<TodoItem> GetItems() => new[]
+        {
+            new TodoItem { Description = "Walk the dog" },
+            new TodoItem { Description = "Buy some almond milk" },
+            new TodoItem { Description = "Learn Avalonia", IsChecked = true },
+        };
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Models;
+using Todo.Services;
+
+namespace Todo.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel(Database db)
+        {
+            List = new TodoListViewModel(db.GetItems());
+        }
+
+        public TodoListViewModel List { get; }
+    }
+}
